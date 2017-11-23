ad6347034d6dc1a5b9d4c1c78d73a192b56d535d
vitorgusta <vitor.garcia72292@outlook.com>
2017-11-23 00:43:24 -0200

    image

--- cup-simulator/controller/image.go
+++ cup-simulator/controller/image.go
@@ -1,146 +1,65 @@
-// package main
-
-// import "image"
-// import "image/color"
-// import "image/png"
-// import "os"
-
-// func main() {
-// 	// Create an 100 x 50 image
-// 	img := image.NewRGBA(image.Rect(0, 0, 100, 50))
-
-// 	// Draw a red dot at (2, 3)
-// 	img.Set(2, 3, color.RGBA{255, 0, 0, 255})
-
-// 	// Save to out.png
-// 	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
-// 	defer f.Close()
-// 	png.Encode(f, img)
-// }
-
-//
-
-// package main
-
-// import (
-// 	"image"
-// 	"image/draw"
-// 	"image/jpeg"
-// 	"image/png"
-// 	"os"
-// )
-
-// func main() {
-// 	imgb, _ := os.Open("input.jpg")
-// 	img, _ := jpeg.Decode(imgb)
-// 	defer imgb.Close()
-
-// 	wmb, _ := os.Open("watermark.png")
-// 	watermark, _ := png.Decode(wmb)
-// 	defer wmb.Close()
-
-// 	offset := image.Pt(100, 100)
-// 	b := img.Bounds()
-// 	m := image.NewRGBA(b)
-// 	draw.Draw(m, b, img, image.ZP, draw.Src)
-// 	draw.Draw(m, watermark.Bounds().Add(offset), watermark, image.ZP, draw.Over)
-
-// 	imgw, _ := os.Create("output.jpg")
-// 	jpeg.Encode(imgw, m, &jpeg.Options{jpeg.DefaultQuality})
-// 	defer imgw.Close()
-// }
-
-// package main
-
-// import (
-// 	"image"
-// 	"image/color"
-// 	"image/png"
-// 	"os"
-
-// 	"golang.org/x/image/font"
-// 	"golang.org/x/image/font/basicfont"
-// 	"golang.org/x/image/math/fixed"
-// )
-
-// func addLabel(img *image.RGBA, x, y int, label string) {
-// 	col := color.RGBA{200, 100, 0, 255}
-// 	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
-
-// 	d := &font.Drawer{
-// 		Dst:  img,
-// 		Src:  image.NewUniform(col),
-// 		Face: basicfont.Face7x13,
-// 		Dot:  point,
-// 	}
-// 	d.DrawString(label)
-// }
-
-// func main() {
-// 	img := image.NewRGBA(image.Rect(0, 0, 1200, 630))
-
-// 	addLabel(img, 20, 30, "Hello Go")
-
-// 	f, err := os.Create("hello-go.png")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer f.Close()
-// 	if err := png.Encode(f, img); err != nil {
-// 		panic(err)
-// 	}
-// }
-package main
+package controller
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fogleman/gg"
 )
-type mapxy struct{
+
+type mapxy struct {
 	x float64
 	y float64
 }
- 
 
-func main() {
+func generateImage() {
 
-	mapv := []mapxy{
-		{x : 0, y: 0,},
-		{
-			x : 0,
-			y : 100,
+	type opt struct {
+		shortnm      byte
+		longnm, help string
+		needArg      bool
+	}
+
+	var basenameOpts []mapxy
+
+	basenameOpts = []mapxy{
+		mapxy{
+			x: 0,
+			y: 0,
 		},
-		{
+		mapxy{
+			x: 0,
+			y: 100,
+		},
+		mapxy{
 			x: 0,
 			y: 200,
 		},
-		{
+		mapxy{
 			x: 0,
 			y: 300,
 		},
-		{
+		mapxy{
 			x: 100,
 			y: 0,
 		},
-		{
+		mapxy{
 			x: 100,
 			y: 100,
 		},
-		{
+		mapxy{
 			x: 100,
 			y: 200,
 		},
-		{
+		mapxy{
 			x: 100,
 			y: 300,
-		}
-		
+		},
 	}
-
+	fmt.Println("basenameOpts", basenameOpts)
 	const X = 1200
-	const Y = 1200
-	im, err := gg.LoadImage(`C:\Users\vggarcia\go\src\generate-image\cup-simulator\src.jpg`)
+	const Y = 630
+	im, err := gg.LoadImage(`/Users/vitorlevy/Documents/generate-image/cup-simulator/Esporte_Simulador_Copa_compartilhamento.png`)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -149,7 +68,7 @@ func main() {
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 	dc.SetRGB(0, 0, 0)
-	if err := dc.LoadFontFace(`C:\Users\vggarcia\go\src\generate-image\cup-simulator\Roboto-Black.ttf`, 30); err != nil {
+	if err := dc.LoadFontFace(`/Users/vitorlevy/Documents/generate-image/cup-simulator/Roboto-Black.ttf`, 30); err != nil {
 		panic(err)
 	}
 
@@ -178,101 +97,3 @@ func main() {
 	dc.Clip()
 	dc.SavePNG("out.png")
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"image"
-// )
-
-// func main() {
-
-// 	r := image.Rect(2, 1, 5, 5)
-// 	// Dx and Dy return a rectangle's width and height.
-
-// 	r.DrawStringAnchored
-// 	fmt.Println(r.Dx(), r.Dy(), image.Pt(0, 0).In(r)) // prints 3 4 false
-// }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"image"
-// 	"image/color"
-// 	"image/jpeg"
-// 	"image/png"
-// 	"os"
-
-// 	"golang.org/x/image/font"
-// 	"golang.org/x/image/font/basicfont"
-// 	"golang.org/x/image/math/fixed"
-// )
-
-// func init() {
-// 	// damn important or else At(), Bounds() functions will
-// 	// caused memory pointer error!!
-// 	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
-// }
-
-// func main() {
-// 	imgfile, err := os.Open(`C:\Users\vggarcia\go\src\generate-image\cup-simulator\src.jpg`)
-
-// 	if err != nil {
-// 		fmt.Println("img.jpg file not found!")
-// 		os.Exit(1)
-// 	}
-
-// 	defer imgfile.Close()
-
-// 	// get image height and width with image/jpeg
-// 	// change accordinly if file is png or gif
-
-// 	imgCfg, _, err := image.DecodeConfig(imgfile)
-
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		os.Exit(1)
-// 	}
-
-// 	width := imgCfg.Width
-// 	height := imgCfg.Height
-
-// 	fmt.Println("Width : ", width)
-// 	fmt.Println("Height : ", height)
-
-// 	// we need to reset the io.Reader again for image.Decode() function below to work
-// 	// otherwise we will  - panic: runtime error: invalid memory address or nil pointer dereference
-// 	// there is no build in rewind for io.Reader, use Seek(0,0)
-// 	imgfile.Seek(0, 0)
-
-// 	// get the image
-// 	img, _, err := image.Decode(imgfile)
-// 	b := img.Bounds()
-// 	imgSet := image.NewRGBA(b)
-// 	addLabel(imgSet, 20, 30, "Hello Go")
-
-// 	f, err := os.Create("hello-goss.png")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer f.Close()
-// 	if err := png.Encode(f, img); err != nil {
-// 		panic(err)
-// 	}
-
-// }
-// func addLabel(img *image.RGBA, x, y int, label string) {
-
-// 	col := color.RGBA{0xdd, 0x00, 0x93, 0xff}
-// 	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
-
-// 	d := &font.Drawer{
-// 		Dst:  img,
-// 		Src:  image.NewUniform(col),
-// 		Face: basicfont.Face7x13,
-// 		Dot:  point,
-// 	}
-// 	d.DrawString(label)
-// }
--- cup-simulator/main.go
+++ cup-simulator/main.go
@@ -1,61 +1,122 @@
-// package main
+package main
 
-// import (
-// 	"flag"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"time"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
-// 	"github.com/labstack/echo"
-// 	"github.com/tylerb/graceful"
-// )
+	"github.com/fogleman/gg"
+	"github.com/labstack/echo"
+	"github.com/tylerb/graceful"
+)
 
-// type Groups struct {
-// 	group []Group `json:"groups"`
-// }
+type mapxy struct {
+	x float64
+	y float64
+}
 
-// type Team struct {
-// 	Name  string `json:"name"`
-// 	Image string `json:"image"`
-// }
+type Groups struct {
+	Groups [][]struct {
+		Image string `json:"image"`
+		Name  string `json:"name"`
+	} `json:"groups"`
+}
 
-// type Group struct {
-// 	team []Team
-// }
+func main() {
 
-// type MyJsonName struct {
-// 	Groups [][]struct {
-// 		Image string `json:"image"`
-// 		Name  string `json:"name"`
-// 	} `json:"groups"`
-// }
+	var port = flag.String("p", "8001", "server port number")
 
-// func main() {
+	flag.Parse()
 
-// 	var port = flag.String("p", "8001", "server port number")
+	log.Printf("Starting Rest API service on port %s\n", *port)
 
-// 	flag.Parse()
+	e := echo.New()
+	e.Server.Addr = ":" + *port
 
-// 	log.Printf("Starting Rest API service on port %s\n", *port)
+	r := e.Group("/api/v1")
+	r.POST("/something", generateTemplate)
 
-// 	e := echo.New()
-// 	e.Server.Addr = ":" + *port
+	graceful.ListenAndServe(e.Server, 5*time.Second)
+}
 
-// 	r := e.Group("/api/v1")
-// 	r.POST("/something", generateTemplate)
+func generateTemplate(c echo.Context) (err error) {
 
-// 	graceful.ListenAndServe(e.Server, 5*time.Second)
-// }
+	groups1 := new(Groups)
 
-// func generateTemplate(c echo.Context) (err error) {
+	if err := c.Bind(groups1); err != nil {
+		return c.JSON(http.StatusForbidden, err)
+	}
 
-// 	groups := new(MyJsonName)
+	img := generateImage(*groups1)
+	fmt.Println("JSON", groups1)
 
-// 	if err := c.Bind(groups); err != nil {
-// 		return c.JSON(http.StatusForbidden, err)
-// 	}
-// 	fmt.Println("JSON", groups)
+	return c.JSON(http.StatusCreated, img)
+}
 
-// 	return c.JSON(http.StatusCreated, groups)
-// }
+func generateImage(groups Groups) string {
+	var basenameOpts []mapxy
+	basenameOpts = []mapxy{
+		mapxy{
+			x: 150,
+			y: 230,
+		},
+		mapxy{
+			x: 0,
+			y: 100,
+		},
+		mapxy{
+			x: 0,
+			y: 200,
+		},
+		mapxy{
+			x: 0,
+			y: 300,
+		},
+		mapxy{
+			x: 100,
+			y: 0,
+		},
+		mapxy{
+			x: 100,
+			y: 100,
+		},
+		mapxy{
+			x: 100,
+			y: 200,
+		},
+		mapxy{
+			x: 100,
+			y: 300,
+		},
+	}
+	const X = 1200
+	const Y = 630
+	im, err := gg.LoadImage(`/Users/vitorlevy/Documents/generate-image/cup-simulator/Esporte_Simulador_Copa_compartilhamento.png`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dc := gg.NewContext(X, Y)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	dc.SetRGB(0, 0, 0)
+	if err := dc.LoadFontFace(`/Users/vitorlevy/Documents/generate-image/cup-simulator/FiraSans-Medium.ttf`, 30); err != nil {
+		panic(err)
+	}
+
+	dc.DrawRoundedRectangle(0, 0, 512, 512, 0)
+	dc.DrawImage(im, 0, 0)
+
+	fmt.Println(basenameOpts)
+	for i, group := range groups.Groups {
+		for _, time := range group {
+			fmt.Println("vitor", i, time.Image, time.Image)
+			dc.DrawStringAnchored(time.Name, 250, 330, 0.5, 0.5)
+		}
+	}
+	dc.Clip()
+	dc.SavePNG("out1.png")
+	return "umg"
+}
